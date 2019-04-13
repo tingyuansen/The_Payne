79cc0931317b1c7a18153dea2f56f95add1b9cb5
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-04-13 11:23:12 -0400

    update

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='The_Payne',
       url='https://github.com/tingyuansen/The_Payne',
       package_dir = {},
       packages=['The_Payne'],
-      package_data={},
+      package_data={'The_Payne':['other_data/*.npz','neural_nets/*.npz']},
       dependency_links = [],
       install_requires=[])
