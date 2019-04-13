a932979b025d3d968dc956af69058404325a5273
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-04-13 09:54:06 -0400

    update

---
+++ The_Payne/process_spectra.py
@@ -0,0 +1,96 @@
+'''
+Code for reading in combined spectra.
+This uses Jo Bovy's APOGEE package to read in spectra: https://github.com/jobovy/apogee
+Any way that you can get your hands on the spectra should be fine, as long as you 
+
+Here we adopt APOGEE DR14. Edit os.environs below for a later version of APOGEE data release.
+Since our spectral model training set was normalized using the DR12 wavelength definition, 
+even thought the spectra are from DR14, we will resample them into DR12 wavelength format.
+'''
+
+from __future__ import absolute_import, division, print_function # python2 compatibility
+import numpy as np
+import sys
+import os
+from . import utils
+from . import spectral_model
+
+os.environ["SDSS_LOCAL_SAS_MIRROR"] = "data.sdss3.org/sas/"
+os.environ["RESULTS_VERS"] = "l31c.2" #v603 for DR12, l30e.2 for DR13, l31c.2 for DR14
+os.environ["APOGEE_APOKASC_REDUX"] = "v6.2a"
+
+from apogee.tools import toAspcapGrid
+import apogee.tools.read as apread
+
+
+# read in the default wavelength array and the list of pixels used for fitting
+wavelength = utils.load_wavelength_array()
+cont_pixels = utils.load_cannon_contpixels()
+
+def read_apogee_catalog():
+    '''
+    read in the catalog of info for all stars in a data release. 
+    '''
+    all_star_catalog = apread.allStar(rmcommissioning = False, rmdups = False, 
+        main = False, raw = True)
+    catalog_id = all_star_catalog['APOGEE_ID'].astype("str")
+    return all_star_catalog, catalog_id
+
+def get_combined_spectrum_single_object(apogee_id, catalog = None, save_local = False):
+    '''
+    apogee_id should be a byte-like object; i.e b'2M13012770+5754582'
+    This downloads a single combined spectrum and the associated error array,
+        and it normalizes both. 
+    '''
+    
+    # read in the allStar catalog if you haven't already
+    if catalog is None:
+        catalog, catalog_id = read_apogee_catalog()
+    
+    _COMBINED_INDEX = 1
+    
+    msk = np.where(catalog_id == apogee_id)[0]
+    if not len(msk):
+        raise ValueError('the desired Apogee ID was not found in the allStar catalog.')
+
+    field = catalog['FIELD'][msk[0]]
+    loc_id = catalog['LOCATION_ID'][msk[0]]
+        
+    if loc_id == 1:
+        temp1 = apread.apStar(field, apogee_id, ext = 1, header = False, aspcapWavegrid = False)
+        temp2 = apread.apStar(field, apogee_id, ext = 2, header = False, aspcapWavegrid = False)
+        temp3 = apread.apStar(field, apogee_id, ext = 3, header = False, aspcapWavegrid = False)
+    else:
+        temp1 = apread.apStar(loc_id, apogee_id, ext = 1, header = False, aspcapWavegrid = False)
+        temp2 = apread.apStar(loc_id, apogee_id, ext = 2, header = False, aspcapWavegrid = False)
+        temp3 = apread.apStar(loc_id, apogee_id, ext = 3, header = False, aspcapWavegrid = False)
+
+    if temp1.shape[0] > 6000:
+        spec = temp1
+        specerr = temp2
+        mask = temp3
+    else:
+        spec = temp1[_COMBINED_INDEX]
+        specerr = temp2[_COMBINED_INDEX]
+        mask = temp3[_COMBINED_INDEX]
+
+    spec = toAspcapGrid(spec, dr='12') # dr12 wavelength format
+    specerr = toAspcapGrid(specerr, dr='12')
+    
+    # cull dead pixels
+    choose = spec <= 0
+    spec[choose] = 0.01
+    specerr[choose] = np.max(np.abs(spec))*999.
+        
+    # continuum-normalize
+    cont = utils.get_apogee_continuum(wavelength = wavelength, spec = spec, 
+        spec_err = specerr, cont_pixels = cont_pixels)
+    spec /= cont
+    specerr /= cont
+    
+    if save_local:
+        np.savez('spectra/combined/spectrum_ap_id_' + str(apogee_id.decode()) + '_.npz',
+                 spectrum = spec, spec_err = specerr)
+    return spec, specerr
+    
+
---
+++ The_Payne/utils.py
@@ -0,0 +1,116 @@
+# a few low-level functions that are used throughout
+from __future__ import absolute_import, division, print_function # python2 compatibility
+import numpy as np
+import os
+
+def read_in_neural_network():
+    '''
+    read in the weights and biases parameterizing a particular neural network. 
+    You can read in existing networks from the neural_nets/ directory, or you
+    can train your own networks and edit this function to read them in. 
+    '''
+
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'neural_nets/NN_normalized_spectra.npz')
+    tmp = np.load(path)
+    w_array_0 = tmp["w_array_0"]
+    w_array_1 = tmp["w_array_1"]
+    w_array_2 = tmp["w_array_2"]
+    b_array_0 = tmp["b_array_0"]
+    b_array_1 = tmp["b_array_1"]
+    b_array_2 = tmp["b_array_2"]
+    x_min = tmp["x_min"]
+    x_max = tmp["x_max"]
+    NN_coeffs = (w_array_0, w_array_1, w_array_2, b_array_0, b_array_1, b_array_2, x_min, x_max)
+    tmp.close()
+    return NN_coeffs
+
+def load_wavelength_array():
+    '''
+    read in the default wavelength grid onto which we interpolate all spectra
+    '''
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/apogee_wavelength.npz')
+    tmp = np.load(path)
+    wavelength = tmp['wavelength']
+    tmp.close()
+    return wavelength
+
+def load_apogee_mask():
+    '''
+    read in the pixel mask with which we will omit bad pixels during spectral fitting
+    The mask is made by comparing the tuned Kurucz models to the observed spectra from Arcturus and the Sun from APOGEE. We mask out pixels that show more than 2% of deviations.
+    '''
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/apogee_mask.npz')
+    tmp = np.load(path)
+    mask = tmp['apogee_mask']
+    tmp.close()
+    return mask
+
+def load_cannon_contpixels():
+    '''
+    read in the default list of APOGEE pixels to use for continuum fitting. 
+    These are taken from Melissa Ness' work with the Cannon
+    '''
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/cannon_cont_pixels_apogee.npz')
+    tmp = np.load(path)
+    pixels_cannon = tmp['pixels_cannon']
+    tmp.close()
+    return pixels_cannon
+    
+def doppler_shift(wavelength, flux, dv):
+    '''
+    dv is in km/s
+    We use the convention where a positive dv means the object is moving away.
+    
+    This linear interpolation is actually not that accurate, but is fine if you 
+    only care about accuracy to the level of a few tenths of a km/s. If you care
+    about better accuracy, you can do better with spline interpolation. 
+    '''
+    c = 2.99792458e5 # km/s
+    doppler_factor = np.sqrt((1 - dv/c)/(1 + dv/c)) 
+    new_wavelength = wavelength * doppler_factor
+    new_flux = np.interp(new_wavelength, wavelength, flux)
+    return new_flux
+
+def get_apogee_continuum(wavelength, spec, spec_err = None, cont_pixels = None):
+    '''
+    this is designed to give the same result as the normalization function from 
+    Jo Bovy's APOGEE package, but it's much faster. 
+    pixels with large uncertainty are weighted less in the fit. 
+    '''
+    if cont_pixels is None:
+        cont_pixels = load_cannon_contpixels()
+    cont = np.empty_like(spec)
+    
+    deg = 4
+    
+    # if we haven't given any uncertainties, just assume they're the same everywhere. 
+    if spec_err is None:
+        spec_err = np.zeros(spec.shape[0]) + 0.0001
+    
+    # Rescale wavelengths
+    bluewav = 2*np.arange(2920)/2919 - 1
+    greenwav = 2*np.arange(2400)/2399 - 1
+    redwav = 2*np.arange(1894)/1893 - 1
+    
+    blue_pixels= cont_pixels[:2920]
+    green_pixels= cont_pixels[2920:5320]
+    red_pixels= cont_pixels[5320:]
+    
+    # blue
+    cont[:2920]= _fit_cannonpixels(bluewav, spec[:2920], spec_err[:2920],
+                        deg, blue_pixels)
+    # green 
+    cont[2920:5320]= _fit_cannonpixels(greenwav, spec[2920:5320], spec_err[2920:5320],
+                        deg, green_pixels)
+    # red
+    cont[5320:]= _fit_cannonpixels(redwav, spec[5320:], spec_err[5320:], deg, red_pixels)
+    return cont
+
+def _fit_cannonpixels(wav, spec, specerr, deg, cont_pixels):
+    '''
+    Fit the continuum to a set of continuum pixels
+    helper function for get_apogee_continuum()
+    '''
+    chpoly = np.polynomial.Chebyshev.fit(wav[cont_pixels], spec[cont_pixels],
+                deg, w=1./specerr[cont_pixels])
+    return chpoly(wav)
