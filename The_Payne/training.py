f6d88b40c290c7a92f6c48523f792535e07ff822
Yuan-Sen Ting <yting@yuan-sens-mbp-3.wireless.ias.edu>
2019-08-26 11:56:36 -0400

    Adopt L^0.8 norm

--- The_Payne/training.py
+++ The_Payne/training.py
@@ -138,7 +138,7 @@ def neural_net(training_labels, training_spectra, validation_labels, validation_
             idx = perm[i * batch_size : (i+1) * batch_size]
             y_pred = model(x[idx])
             #loss = loss_fn(y_pred, y[idx])*1e4
-            loss = (y_pred-y[idx]).abs().pow(1.5).mean()*1e4
+            loss = (y_pred-y[idx]).abs().pow(0.8).mean()*1e4
             optimizer.zero_grad()
             loss.backward(retain_graph=True)
             optimizer.step()
@@ -147,7 +147,7 @@ def neural_net(training_labels, training_spectra, validation_labels, validation_
         if e % 10 == 0:
             y_pred_valid = model(x_valid)
             #loss_valid = loss_fn(y_pred_valid, y_valid)*1e4
-            loss_valid = (y_pred_valid-y_valid).abs().pow(1.5).mean()*1e4
+            loss_valid = (y_pred_valid-y_valid).abs().pow(0.8).mean()*1e4
             print('iter %s:' % e, 'training loss = %.3f' % loss,\
                  'validation loss = %.3f' % loss_valid)
 
