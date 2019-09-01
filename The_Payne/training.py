d528951834b9155cc97f1c96d6ddad6b9996869c
Yuan-Sen Ting <yting@yuan-sens-mbp-3.wireless.ias.edu>
2019-09-01 14:19:38 -0400

    Update

--- The_Payne/training.py
+++ The_Payne/training.py
@@ -253,19 +253,19 @@ def neural_net(training_labels, training_spectra, validation_labels, validation_
             print('iter %s:' % e, 'training loss = %.3f' % loss,\
                  'validation loss = %.3f' % loss_valid)
 
-            loss_data = loss.data.item()
-            loss_valid_data = loss_valid.data.item()
-            training_loss.append(loss_data)
-            validation_loss.append(loss_valid_data)
+            #loss_data = loss.data.item()
+            #loss_valid_data = loss_valid.data.item()
+            #training_loss.append(loss_data)
+            #validation_loss.append(loss_valid_data)
 
             # record the weights and biases if the validation loss improves
             if loss_valid_data < current_loss:
                 current_loss = loss_valid
                 torch.save(model, 'NN_normalized_spectra.pt')
 
-                np.savez("training_loss.npz",\
-                         training_loss = training_loss,\
-                         validation_loss = validation_loss)
+                #np.savez("training_loss.npz",\
+                #         training_loss = training_loss,\
+                #         validation_loss = validation_loss)
 
                 #model_numpy = []
                 #for param in model.parameters():
