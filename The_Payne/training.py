92445bcbc14287fa4a8ebd40e542c0d0c8598986
Yuan-Sen Ting <yting@yuan-sens-mbp-2.wireless.ias.edu>
2019-09-06 18:27:54 -0400

    Update

--- The_Payne/training.py
+++ The_Payne/training.py
@@ -33,8 +33,8 @@ class Payne_model(torch.nn.Module):
             torch.nn.Linear(num_neurons, num_features),
         )
 
-        self.deconv1 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
-        self.deconv2 = torch.nn.ConvTranspose1d(64, 128, mask_size, stride=3, padding=5)
+        self.deconv1 = torch.nn.ConvTranspose1d(64, 128, mask_size, stride=3, padding=5)
+        self.deconv2 = torch.nn.ConvTranspose1d(128, 128, mask_size, stride=3, padding=5)
         self.deconv3 = torch.nn.ConvTranspose1d(128, 128, mask_size, stride=3, padding=5)
         self.deconv4 = torch.nn.ConvTranspose1d(128, 128, mask_size, stride=3, padding=5)
         self.deconv5 = torch.nn.ConvTranspose1d(128, 128, mask_size, stride=3, padding=5)
@@ -42,7 +42,7 @@ class Payne_model(torch.nn.Module):
         self.deconv7 = torch.nn.ConvTranspose1d(64, 1, mask_size, stride=3, padding=5)
 
         self.deconv2b = torch.nn.Sequential(
-                            torch.nn.ConvTranspose1d(64, 128, 1, stride=3),\
+                            torch.nn.ConvTranspose1d(128, 128, 1, stride=3),\
                             torch.nn.BatchNorm1d(128)
                         )
         self.deconv3b = torch.nn.Sequential(
@@ -63,7 +63,7 @@ class Payne_model(torch.nn.Module):
                         )
 
         self.batch_norm1 = torch.nn.Sequential(
-                            torch.nn.BatchNorm1d(64),
+                            torch.nn.BatchNorm1d(128),
                             torch.nn.LeakyReLU()
         )
         self.batch_norm2 = torch.nn.Sequential(
