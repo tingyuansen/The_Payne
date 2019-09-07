612fe463d4cb4ee9d80fdc9e77645feae376a77c
Yuan-Sen Ting <yting@Yuan-Sens-MacBook-Pro-2.local>
2019-09-07 17:52:54 -0400

    Update

--- The_Payne/training.py
+++ The_Payne/training.py
@@ -36,8 +36,8 @@ class Payne_model(torch.nn.Module):
         self.deconv1 = torch.nn.ConvTranspose1d(128, 128, mask_size, stride=3, padding=5)
         self.deconv2 = torch.nn.ConvTranspose1d(128, 128, mask_size, stride=3, padding=5)
         self.deconv3 = torch.nn.ConvTranspose1d(128, 128, mask_size, stride=3, padding=5)
-        self.deconv4 = torch.nn.ConvTranspose1d(128, 128, mask_size, stride=3, padding=5)
-        self.deconv5 = torch.nn.ConvTranspose1d(128, 64, mask_size, stride=3, padding=5)
+        self.deconv4 = torch.nn.ConvTranspose1d(128, 64, mask_size, stride=3, padding=5)
+        self.deconv5 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
         self.deconv6 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
         self.deconv7 = torch.nn.ConvTranspose1d(64, 1, mask_size, stride=3, padding=5)
 
@@ -50,11 +50,11 @@ class Payne_model(torch.nn.Module):
                             torch.nn.BatchNorm1d(128)
                         )
         self.deconv4b = torch.nn.Sequential(
-                            torch.nn.ConvTranspose1d(128, 128, 1, stride=3),\
-                            torch.nn.BatchNorm1d(128)
+                            torch.nn.ConvTranspose1d(128, 64, 1, stride=3),\
+                            torch.nn.BatchNorm1d(64)
                         )
         self.deconv5b = torch.nn.Sequential(
-                            torch.nn.ConvTranspose1d(128, 64, 1, stride=3),\
+                            torch.nn.ConvTranspose1d(64, 64, 1, stride=3),\
                             torch.nn.BatchNorm1d(64)
                         )
         self.deconv6b = torch.nn.Sequential(
@@ -75,7 +75,7 @@ class Payne_model(torch.nn.Module):
                             torch.nn.LeakyReLU()
         )
         self.batch_norm4 = torch.nn.Sequential(
-                            torch.nn.BatchNorm1d(128),
+                            torch.nn.BatchNorm1d(64),
                             torch.nn.LeakyReLU()
         )
         self.batch_norm5 = torch.nn.Sequential(
