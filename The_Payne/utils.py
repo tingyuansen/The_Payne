cb0d62532084b1215152e8b2ff05891b82cf6d51
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-04 20:21:46 -0500

    Update

--- The_Payne/utils.py
+++ The_Payne/utils.py
@@ -3,11 +3,12 @@ from __future__ import absolute_import, division, print_function # python2 compa
 import numpy as np
 import os
 
+
 def read_in_neural_network():
     '''
-    read in the weights and biases parameterizing a particular neural network. 
+    read in the weights and biases parameterizing a particular neural network.
     You can read in existing networks from the neural_nets/ directory, or you
-    can train your own networks and edit this function to read them in. 
+    can train your own networks and edit this function to read them in.
     '''
 
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'neural_nets/NN_normalized_spectra.npz')
@@ -24,6 +25,7 @@ def read_in_neural_network():
     tmp.close()
     return NN_coeffs
 
+
 def load_wavelength_array():
     '''
     read in the default wavelength grid onto which we interpolate all spectra
@@ -34,10 +36,12 @@ def load_wavelength_array():
     tmp.close()
     return wavelength
 
+
 def load_apogee_mask():
     '''
     read in the pixel mask with which we will omit bad pixels during spectral fitting
-    The mask is made by comparing the tuned Kurucz models to the observed spectra from Arcturus and the Sun from APOGEE. We mask out pixels that show more than 2% of deviations.
+    The mask is made by comparing the tuned Kurucz models to the observed spectra from Arcturus
+    and the Sun from APOGEE. We mask out pixels that show more than 2% of deviations.
     '''
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/apogee_mask.npz')
     tmp = np.load(path)
@@ -45,10 +49,10 @@ def load_apogee_mask():
     tmp.close()
     return mask
 
+
 def load_cannon_contpixels():
     '''
-    read in the default list of APOGEE pixels to use for continuum fitting. 
-    These are taken from Melissa Ness' work with the Cannon
+    read in the default list of APOGEE pixels for continuum fitting.
     '''
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/cannon_cont_pixels_apogee.npz')
     tmp = np.load(path)
@@ -56,14 +60,14 @@ def load_cannon_contpixels():
     tmp.close()
     return pixels_cannon
 
+
 def load_training_data():
     '''
     read in the default Kurucz training spectra for APOGEE
 
-    Here we only consider 800 training spectra and 200 validation spectra
-    for the tutorial (due to the GitHub upload limit); in practice, more
-    training spectra will be better. The default neural networks included were
-    trained using 10000 training spectra.
+    Here we only consider a small number (<1000) of training spectra.
+    In practice, more training spectra will be better. The default
+    neural network was trained using 12000 training spectra.
     '''
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/kurucz_training_spectra.npz')
     tmp = np.load(path)
@@ -74,57 +78,59 @@ def load_training_data():
     tmp.close()
     return training_labels, training_spectra, validation_labels, validation_spectra
 
+
 def doppler_shift(wavelength, flux, dv):
     '''
     dv is in km/s
     We use the convention where a positive dv means the object is moving away.
-    
-    This linear interpolation is actually not that accurate, but is fine if you 
+
+    This linear interpolation is actually not that accurate, but is fine if you
     only care about accuracy to the level of a few tenths of a km/s. If you care
-    about better accuracy, you can do better with spline interpolation. 
+    about better accuracy, you can do better with spline interpolation.
     '''
     c = 2.99792458e5 # km/s
-    doppler_factor = np.sqrt((1 - dv/c)/(1 + dv/c)) 
+    doppler_factor = np.sqrt((1 - dv/c)/(1 + dv/c))
     new_wavelength = wavelength * doppler_factor
     new_flux = np.interp(new_wavelength, wavelength, flux)
     return new_flux
 
+
 def get_apogee_continuum(spec, spec_err = None, cont_pixels = None):
     '''
-    this is designed to give the same result as the normalization function from 
-    Jo Bovy's APOGEE package, but it's much faster. 
-    pixels with large uncertainty are weighted less in the fit. 
+    continuum normalize spectrum.
+    pixels with large uncertainty are weighted less in the fit.
     '''
     if cont_pixels is None:
         cont_pixels = load_cannon_contpixels()
     cont = np.empty_like(spec)
 
     wavelength = load_wavelength_array()
-    
+
     deg = 4
-    
-    # if we haven't given any uncertainties, just assume they're the same everywhere. 
+
+    # if we haven't given any uncertainties, just assume they're the same everywhere.
     if spec_err is None:
         spec_err = np.zeros(spec.shape[0]) + 0.0001
-    
+
     # Rescale wavelengths
     bluewav = 2*np.arange(2920)/2919 - 1
     greenwav = 2*np.arange(2400)/2399 - 1
     redwav = 2*np.arange(1894)/1893 - 1
-    
+
     blue_pixels= cont_pixels[:2920]
     green_pixels= cont_pixels[2920:5320]
     red_pixels= cont_pixels[5320:]
-    
+
     # blue
     cont[:2920]= _fit_cannonpixels(bluewav, spec[:2920], spec_err[:2920],
                         deg, blue_pixels)
-    # green 
+    # green
     cont[2920:5320]= _fit_cannonpixels(greenwav, spec[2920:5320], spec_err[2920:5320],
                         deg, green_pixels)
     # red
     cont[5320:]= _fit_cannonpixels(redwav, spec[5320:], spec_err[5320:], deg, red_pixels)
     return cont
+
 
 def _fit_cannonpixels(wav, spec, specerr, deg, cont_pixels):
     '''
