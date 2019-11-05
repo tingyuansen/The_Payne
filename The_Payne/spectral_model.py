ed4c7bd3dc31416218d439403a499f46b98972af
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2019-11-04 19:31:47 -0500

    Update

--- The_Payne/spectral_model.py
+++ The_Payne/spectral_model.py
@@ -3,32 +3,22 @@ from __future__ import absolute_import, division, print_function # python2 compa
 import numpy as np
 from . import utils
 
-def sigmoid(z):
+def leaky_relu(z):
     '''
     This is the activation function used by default in all our neural networks.
-    You can experiment with using an ReLU instead, but I got worse results in
-    some simple tests.
     '''
-    #return 1.0/(1.0 + np.exp(-z))
     return z*(z > 0) + 0.01*z*(z < 0)
-
+    
 def get_spectrum_from_neural_net(scaled_labels, NN_coeffs):
     '''
     Predict the rest-frame spectrum (normalized) of a single star.
-    We input the scaled stellar labels (not in the original unit). Each label ranges from -0.5 to 0.5
+    We input the scaled stellar labels (not in the original unit).
+    Each label ranges from -0.5 to 0.5
     '''
 
     # assuming your NN has two hidden layers.
     w_array_0, w_array_1, w_array_2, b_array_0, b_array_1, b_array_2, x_min, x_max = NN_coeffs
-
-    # the neural network architecture adopted in Ting+ 18, individual networks for individual pixels
-    #inside = np.einsum('ijk,k->ij', w_array_0, scaled_labels) + b_array_0
-    #outside = np.einsum('ik,ik->i', w_array_1, sigmoid(inside)) + b_array_1
-    #spectrum = w_array_2*sigmoid(outside) + b_array_2
-
-    # having a single large network seems for all pixels seems to work better
-    # as it exploits the information between adjacent pixels
     inside = np.einsum('ij,j->i', w_array_0, scaled_labels) + b_array_0
-    outside = np.einsum('ij,j->i', w_array_1, sigmoid(inside)) + b_array_1
-    spectrum = np.einsum('ij,j->i', w_array_2, sigmoid(outside)) + b_array_2
+    outside = np.einsum('ij,j->i', w_array_1, leaky_relu(inside)) + b_array_1
+    spectrum = np.einsum('ij,j->i', w_array_2, leak_relu(outside)) + b_array_2
     return spectrum
