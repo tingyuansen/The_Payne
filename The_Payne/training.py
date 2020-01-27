a2208b397ab92dddef6f8bb0c2f3c9fbb2b3b6f0
Yuan-Sen Ting <ting.yuansen.astro@gmail.com>
2020-01-27 18:16:49 -0500

    Update

--- The_Payne/training.py
+++ The_Payne/training.py
@@ -46,66 +46,100 @@ from . import radam
 
 #---------------------------------------------------------------------------------------------------
 # resnet models
-class Payne_model(torch.nn.Module):
-    def __init__(self, dim_in, num_neurons, num_features, mask_size, num_pixel):
+# class Payne_model(torch.nn.Module):
+#     def __init__(self, dim_in, num_neurons, num_features, mask_size, num_pixel):
+#         super(Payne_model, self).__init__()
+#         self.features = torch.nn.Sequential(
+#             torch.nn.Linear(dim_in, num_neurons),
+#             torch.nn.LeakyReLU(),
+#             torch.nn.Linear(num_neurons, num_neurons),
+#             torch.nn.LeakyReLU(),
+#             torch.nn.Linear(num_neurons, num_features),
+#         )
+#
+#         self.deconv1 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
+#         self.deconv2 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
+#         self.deconv3 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
+#         self.deconv4 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
+#         self.deconv5 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
+#         self.deconv6 = torch.nn.ConvTranspose1d(64, 32, mask_size, stride=3, padding=5)
+#         self.deconv7 = torch.nn.ConvTranspose1d(32, 1, mask_size, stride=3, padding=5)
+#
+#         self.deconv2b = torch.nn.ConvTranspose1d(64, 64, 1, stride=3)
+#         self.deconv3b = torch.nn.ConvTranspose1d(64, 64, 1, stride=3)
+#         self.deconv4b = torch.nn.ConvTranspose1d(64, 64, 1, stride=3)
+#         self.deconv5b = torch.nn.ConvTranspose1d(64, 64, 1, stride=3)
+#         self.deconv6b = torch.nn.ConvTranspose1d(64, 32, 1, stride=3)
+#
+#         self.relu2 = torch.nn.LeakyReLU()
+#         self.relu3 = torch.nn.LeakyReLU()
+#         self.relu4 = torch.nn.LeakyReLU()
+#         self.relu5 = torch.nn.LeakyReLU()
+#         self.relu6 = torch.nn.LeakyReLU()
+#
+#         self.num_pixel = num_pixel
+#
+#     def forward(self, x):
+#         x = self.features(x)[:,None,:]
+#         x = x.view(x.shape[0], 64, 3)
+#         x1 = self.deconv1(x)
+#
+#         x2 = self.deconv2(x1)
+#         x2 += self.deconv2b(x1)
+#         x2 = self.relu2(x2)
+#
+#         x3 = self.deconv3(x2)
+#         x3 += self.deconv3b(x2)
+#         x3 = self.relu2(x3)
+#
+#         x4 = self.deconv4(x3)
+#         x4 += self.deconv4b(x3)
+#         x4 = self.relu2(x4)
+#
+#         x5 = self.deconv5(x4)
+#         x5 += self.deconv5b(x4)
+#         x5 = self.relu2(x5)
+#
+#         x6 = self.deconv6(x5)
+#         x6 += self.deconv6b(x5)
+#         x6 = self.relu2(x6)
+#
+#         x7 = self.deconv7(x6)[:,0,:self.num_pixel]
+#         return x7
+
+
+#---------------------------------------------------------------------------------------------------
+# define network
+class Payne_model(nn.Module):
+    def __init__(self, dim_in, num_pixel):
         super(Payne_model, self).__init__()
-        self.features = torch.nn.Sequential(
-            torch.nn.Linear(dim_in, num_neurons),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(num_neurons, num_neurons),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(num_neurons, num_features),
-        )
+        self.z_dim = dim_in
+        layers = []
+        channel = 256
 
-        self.deconv1 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
-        self.deconv2 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
-        self.deconv3 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
-        self.deconv4 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
-        self.deconv5 = torch.nn.ConvTranspose1d(64, 64, mask_size, stride=3, padding=5)
-        self.deconv6 = torch.nn.ConvTranspose1d(64, 32, mask_size, stride=3, padding=5)
-        self.deconv7 = torch.nn.ConvTranspose1d(32, 1, mask_size, stride=3, padding=5)
+        for i in range(6):
+            for j in range(2):
 
-        self.deconv2b = torch.nn.ConvTranspose1d(64, 64, 1, stride=3)
-        self.deconv3b = torch.nn.ConvTranspose1d(64, 64, 1, stride=3)
-        self.deconv4b = torch.nn.ConvTranspose1d(64, 64, 1, stride=3)
-        self.deconv5b = torch.nn.ConvTranspose1d(64, 64, 1, stride=3)
-        self.deconv6b = torch.nn.ConvTranspose1d(64, 32, 1, stride=3)
+                if i == 0 and j == 0:
+                    layers.append(torch.nn.ConvTranspose1d(z_dim, channel, 7, stride=1))
+                    layers.append(torch.nn.BatchNorm1d(channel, momentum=0.001, affine=False))
+                    layers.append(torch.nn.LeakyReLU(0.2, inplace=True))
+                else:
+                    layers.append(torch.nn.Conv1d(channel, channel, 5, stride=1, padding=2))
+                    layers.append(torch.nn.BatchNorm1d(channel, momentum=0.001, affine=False))
+                    layers.append(torch.nn.LeakyReLU(0.2, inplace=True))
 
-        self.relu2 = torch.nn.LeakyReLU()
-        self.relu3 = torch.nn.LeakyReLU()
-        self.relu4 = torch.nn.LeakyReLU()
-        self.relu5 = torch.nn.LeakyReLU()
-        self.relu6 = torch.nn.LeakyReLU()
+            if i < 5:
+                layers.append(torch.nn.Upsample(scale_factor=4, mode='linear', align_corners = False))
+            else:
+                layers.append(torch.nn.Conv1d(channel, 1, 6, stride=1))
+                layers.append(torch.nn.LeakyReLU())
 
-        self.num_pixel = num_pixel
+        self.model = torch.nn.Sequential(*layers)
+        self.add_module("model", self.model)
 
-    def forward(self, x):
-        x = self.features(x)[:,None,:]
-        x = x.view(x.shape[0], 64, 3)
-        x1 = self.deconv1(x)
-
-        x2 = self.deconv2(x1)
-        x2 += self.deconv2b(x1)
-        x2 = self.relu2(x2)
-
-        x3 = self.deconv3(x2)
-        x3 += self.deconv3b(x2)
-        x3 = self.relu2(x3)
-
-        x4 = self.deconv4(x3)
-        x4 += self.deconv4b(x3)
-        x4 = self.relu2(x4)
-
-        x5 = self.deconv5(x4)
-        x5 += self.deconv5b(x4)
-        x5 = self.relu2(x5)
-
-        x6 = self.deconv6(x5)
-        x6 += self.deconv6b(x5)
-        x6 = self.relu2(x6)
-
-        x7 = self.deconv7(x6)[:,0,:self.num_pixel]
-        return x7
+    def forward(self, z):
+        return self.model(z).view(-1,7163)
 
 
 #===================================================================================================
